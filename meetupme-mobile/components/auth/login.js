dafa15d9a0cd22bf4b9849ad5d58352b1488be10
Rachel Jacobson <rmjacobson1@gmail.com>
2017-10-15 15:08:40 -0700

    styled login and search

--- meetupme-mobile/components/auth/login.js
+++ meetupme-mobile/components/auth/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Font } from 'expo';
 
 import {
   StyleSheet,
@@ -17,12 +18,22 @@ class Login extends React.Component {
 
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      fontLoaded: false
     };
 
     this.onSignIn = this.onSignIn.bind(this);
     this.onSignUp = this.onSignUp.bind(this);
     this.navigateToMain = this.navigateToMain.bind(this);
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'baloo-chettan': require('../../assets/fonts/BalooChettan-Regular.ttf'),
+      'open-sans': require('../../assets/fonts/OpenSans-Regular.ttf')
+    });
+
+     this.setState({ fontLoaded: true });
   }
 
   componentWillReceiveProps() {
@@ -48,9 +59,17 @@ class Login extends React.Component {
       return (
         <View style={styles.container}>
           <View style={styles.titleContainer}>
-            <Text style={styles.title}>
-              Nau
-              </Text>
+            { this.state.fontLoaded ?
+              (<View>
+                <Text style={styles.title}>
+                  Nau
+                </Text>
+                <Text style={styles.subtitle}>
+                  Get what you need, Nau.
+                </Text>
+              </View>) : null
+
+             }
           </View>
           <View style={styles.inputs}>
             <View style={styles.field}>
@@ -88,17 +107,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'stretch',
-    paddingTop: 20,
+    paddingTop: 40,
+    backgroundColor: '#536A91',
     top: Dimensions.get('window').height*.01,
   },
   titleContainer: {
     padding: 10
   },
   title: {
-    color: '#000053',
-    fontSize: 35,
+    color: '#FF890D',
+    fontSize: 56,
+    fontFamily: 'baloo-chettan',
     top: Dimensions.get('window').height*.05,
-    marginBottom: 20,
+    marginBottom: 10,
+    textAlign: 'center'
+  },
+  subtitle: {
+    color: '#FF890D',
+    fontSize: 26,
+    fontFamily: 'open-sans',
+    top: Dimensions.get('window').height*.05,
+    margin: 5,
     textAlign: 'center'
   },
   inputs: {
@@ -107,28 +136,33 @@ const styles = StyleSheet.create({
   field: {
     borderRadius: 5,
     padding: 5,
-    margin: 20,
-    backgroundColor: 'white',
-    borderBottomColor: '#000053',
-    borderBottomWidth: 1,
-    alignItems: 'center'
+    marginBottom: 15,
+    marginTop: 5,
+    width: '80%',
+    backgroundColor: '#E9ECF1',
+    borderBottomColor: '#183059',
+    borderBottomWidth: 3,
+    alignSelf: 'center'
   },
   textInput: {
     height: 50,
-    color: '#000053',
+    fontSize: 24,
+    color: 'black',
     textAlign: 'center',
     width: '100%'
   },
   buttonContainer: {
     padding: 20,
-    width: '75%',
+    width: '65%',
     alignSelf: 'center',
   },
   login: {
     borderRadius: 5,
     borderColor: 'transparent',
-    backgroundColor: '#000053',
-    padding: 20,
+    backgroundColor: '#183059',
+    marginBottom: 20,
+    padding: 10,
+
   },
   loginText: {
     textAlign: 'center',
